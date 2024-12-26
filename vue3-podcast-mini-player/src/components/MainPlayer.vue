b3c6732e5b78f1c885f7875536a2c9162a0663df
mshafer1 <2565361+mshafer1@users.noreply.github.com>
2024-12-25 21:57:06 -0600

    introduce an app wide max-height

--- vue3-podcast-mini-player/src/components/MainPlayer.vue
+++ vue3-podcast-mini-player/src/components/MainPlayer.vue
@@ -1,38 +1,40 @@
 <template>
-    <div class="row fixed-top">
-        <div class="col-md-1 d-none d-md-block">&nbsp;</div>
-        <div class="col-md-10 col-sm-12">
-            <APlayer :audio="audio" ref="aplayer" loop="false" />
+    <div class="app-height">
+        <div class="row fixed-top">
+            <div class="col-md-1 d-none d-md-block">&nbsp;</div>
+            <div class="col-md-10 col-sm-12">
+                <APlayer :audio="audio" ref="aplayer" loop="false" />
+            </div>
+            <div class="col-md-1 d-none d-md-block">&nbsp;</div>
         </div>
-        <div class="col-md-1 d-none d-md-block">&nbsp;</div>
-    </div>
-    <div style="min-height: 100px;">
+        <div style="min-height: 100px;">
 
-    </div>
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-12 text-center">{{ episodes.length }} episodes available</div>
         </div>
-        <div class="container border">
-            <template v-for="episode in episodes" :key="episode.name">
-                <div class="row pb-1 mt-5">
-                    <div class="col-sm-4">
-                        <b>{{ episode.pubDate }}</b> <br />
-                        {{ episode.name }}
-                    </div>
-                    <div class="col-sm-4">
-                        {{ episode.length }}
-                    </div>
-                    <div class="col-sm-2">
-                        <button class="btn btn-dark p-4 rounded-circle btn-md" :disabled="episode.is_playing"
-                            @click="play(episode)"><i class="bi" :class="episode.icon"></i></button>
-                    </div>
-                    <div class="col-sm-2" v-html="episode.infoButton()">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 text-center">{{ episodes.length }} episodes available</div>
+            </div>
+            <div class="container border">
+                <template v-for="episode in episodes" :key="episode.name">
+                    <div class="row pb-1 mt-5">
+                        <div class="col-sm-4">
+                            <b>{{ episode.pubDate }}</b> <br />
+                            {{ episode.name }}
+                        </div>
+                        <div class="col-sm-4">
+                            {{ episode.length }}
+                        </div>
+                        <div class="col-sm-2">
+                            <button class="btn btn-dark p-4 rounded-circle btn-md" :disabled="episode.is_playing"
+                                @click="play(episode)"><i class="bi" :class="episode.icon"></i></button>
+                        </div>
+                        <div class="col-sm-2" v-html="episode.infoButton()">
 
+                        </div>
                     </div>
-                </div>
-                <div class="pb-3 border-bottom" v-html="episode.summaryCollapsable()"></div>
-            </template>
+                    <div class="pb-3 border-bottom" v-html="episode.summaryCollapsable()"></div>
+                </template>
+            </div>
         </div>
     </div>
 </template>
@@ -54,7 +56,7 @@ class Episode {
         this.cover = image;
         this.pubDate = pubDate;
         this.guid = guid;
-        this.theme='#41b883'
+        this.theme = '#41b883'
 
         this.is_playing = false;
         // this.is_paused=false;
@@ -135,6 +137,11 @@ onMounted(() => {
 <style lang="css">
 button.btn[aria-expanded="true"] {
     /* transform: rotate(90deg); */
-    writing-mode:vertical-rl;
+    writing-mode: vertical-rl;
+}
+
+.app-height {
+    max-height: 500px;
+    overflow-y: scroll;
 }
 </style>
